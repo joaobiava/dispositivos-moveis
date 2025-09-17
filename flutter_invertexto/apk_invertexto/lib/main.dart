7146901d1c80156c0addc55bdfb148d720a8b77b
joaobiava <138793503+joaobiava@users.noreply.github.com>
2025-09-16 21:20:10 -0300

    trabalho api invertexto

---
+++ flutter_invertexto/apk_invertexto/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:apk_invertexto/view/home_page.dart';
+import 'package:flutter/material.dart';
+// meu token supremo = miau = 21574|4OavRPTMbj39IfO9q7wc2xQ8G4H2vaAo
+
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
